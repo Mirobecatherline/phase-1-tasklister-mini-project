6b3426922fc37ed6d8e395d409b570eff81bba35
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-13 20:42:41 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form=document.querySelector('form')
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    handle(e.target.descriptions.value)
+    form.reset()
+  })
 });
+function handle(doing) {
+  let list=document.createElement('li')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',delete1=(e)=>e.target.parentNode.remove())
+  btn.textContent="X"
+  list.textContent=doing;
+  list.appendChild(btn)
+  document.querySelector("#tasks").appendChild(list)
+}
